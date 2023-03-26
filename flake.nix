3c799f771542fb4f9a603761e50d01168a1dec21
Daniel McCarney <daniel@binaryparadox.net>
2023-03-26 13:24:51 -0400

    nix: flake tidy + update.
    * Cuts over from Nixpkgs 22.03 to unstable. Stable's hugo is marked
      broken and with a flake.lock unstable is plenty stable.
    * Cleans up a flake-parts warning RE: `inherit self`.
    * Cleans up a flake warning RE: unused follow.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = { url = "github:hercules-ci/flake-parts"; };
   };
 
-  outputs = { self, nixpkgs, flake-parts, }:
-    flake-parts.lib.mkFlake { inherit self; } {
+  outputs = inputs@{ self, nixpkgs, flake-parts, }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
 
       perSystem = { pkgs, self', ... }: {
